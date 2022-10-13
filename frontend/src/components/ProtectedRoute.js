32948f0c68b529c9afd7133852ef6a33f96872dc
kirillzhakin <kirillzhakin@mail.ru>
2022-10-13 08:42:00 +0500

    ПР15

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ isLoggedIn, children }) => {
-  return isLoggedIn ? children : <Navigate to="/sign-in" />;
-};
-
-export default ProtectedRoute;
